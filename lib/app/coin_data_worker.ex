433c2c8c94b283f1b65b69b4f7a5c4b53766eb61
Jachin <jachin.dev@outlook.com>
2022-05-08 11:19:29 +0200

    add abstract module for getting and cleaning  data from worker

--- lib/app/coin_data_worker.ex
+++ lib/app/coin_data_worker.ex
@@ -3,7 +3,7 @@ defmodule App.CoinDataWorker do
   require Logger
 
   @interval 5_000
-  @coincap_url "https://api.coincap.io/v2/assets/?limit=1"
+  @coincap_url "https://api.coincap.io/v2/assets/?limit=10"
 
   def start_link(initial_state) do
     GenServer.start_link(__MODULE__, initial_state, name: __MODULE__)
@@ -17,29 +17,103 @@ defmodule App.CoinDataWorker do
     else
       {:ok, %{status_code: status_code}} ->
         Logger.error("An error ouccured, status_code: #{status_code}")
-        nil
+        :error
 
       {:error, %{reason: reason}} ->
         Logger.error("An error occured, reason: #{reason}")
-        nil
+        :error
     end
   end
 
-  def init(state) do
-    schedule_coin_data_fetch()
-    {:ok, state}
+  def init(initial_state) do
+    schedule_data_fetch()
+    {:ok, initial_state}
   end
 
   def handle_info(:fetch_from_api, current_state) do
-    {:ok, fetched_data} = fetch_coin_data()
-    new_state = %{current_state | coins_data: fetched_data}
+    new_state =
+      case fetch_coin_data() do
+        {:ok, fetched_data} ->
+          Map.put(current_state, :coins_data, fetched_data)
 
-    schedule_coin_data_fetch()
+        :error ->
+          Map.put(current_state, :coins_data, :error)
+      end
 
+    schedule_data_fetch()
     {:noreply, new_state}
   end
 
-  defp schedule_coin_data_fetch do
+  def handle_call(:get_coins_data, _from, current_state) do
+    {:reply, current_state, current_state}
+  end
+
+  defp schedule_data_fetch do
     Process.send_after(self(), :fetch_from_api, @interval)
+  end
+end
+
+defmodule App.CoinDataServer do
+  def get_all_coins_data do
+    GenServer.call(App.CoinDataWorker, :get_coins_data)
+    |> clean_data()
+  end
+
+  defp clean_data(%{coins_data: coins_data_list} = data)
+       when coins_data_list == [] or is_atom(coins_data_list) do
+    data
+  end
+
+  defp clean_data(%{coins_data: coins_data_list} = data) do
+    cleaned_data =
+      coins_data_list
+      |> Enum.map(fn coin_data_map -> trim_map_values(coin_data_map) end)
+
+    %{data | coins_data: cleaned_data}
+  end
+
+  defp trim_map_values(coin_data_map) do
+    coin_data_map
+    |> Enum.map(fn {key, value} -> parse_and_shorten_value(key, value) end)
+    |> Enum.into(%{})
+  end
+
+  defp parse_and_shorten_value(key, value)
+       when key in [
+              "marketCapUsd",
+              "maxSupply",
+              "supply",
+              "volumeUsd24Hr"
+            ] do
+    readable_value = number_to_human(value)
+    {key, readable_value}
+  end
+
+  defp parse_and_shorten_value(key, value)
+       when key in ["priceUsd", "vwap24Hr", "changePercent24Hr"] do
+    {
+      key,
+      value
+      |> Number.Conversion.to_float()
+      |> Float.floor(2)
+      |> Float.to_string()
+    }
+  end
+
+  defp parse_and_shorten_value(key, value) do
+    {key, value}
+  end
+
+  defp number_to_human(nil) do
+    nil
+  end
+
+  defp number_to_human(value) do
+    [number, prefix] =
+      value
+      |> Number.Human.number_to_human()
+      |> String.split(" ")
+
+    number <> String.at(prefix, 0)
   end
 end
