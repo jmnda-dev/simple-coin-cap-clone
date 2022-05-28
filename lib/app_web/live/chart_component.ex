2442bdc4cbe429d0c57699c41d57c755cf8dd652
Jachin <jachin.dev@outlook.com>
2022-05-28 17:39:33 +0200

    add width

--- lib/app_web/live/chart_component.ex
+++ lib/app_web/live/chart_component.ex
@@ -4,9 +4,9 @@ defmodule AppWeb.ChartComponent do
 
   def chart(assigns) do
     ~H"""
-    <p>hello</p>
-
-    <canvas phx-hook="Chart" id="myChart"></canvas>
+    <div class="md:w-4/5 w-full">
+      <canvas phx-hook="Chart" id="myChart"></canvas>
+    </div>
     """
   end
 end
