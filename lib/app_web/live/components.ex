f78660bc69210d882a4303cfaa63a0336e5724e7
Jachin Manda <jachin@outlook.com>
2022-06-26 16:35:55 +0200

    move to conponent module

--- lib/app_web/live/components.ex
+++ lib/app_web/live/components.ex
@@ -1,5 +1,14 @@
-defmodule AppWeb.SpinnerComponent do
+defmodule AppWeb.Components do
   use Phoenix.Component
+  use Phoenix.HTML
+
+  def chart(assigns) do
+    ~H"""
+    <div class="md:w-4/5 w-full">
+      <canvas phx-hook="Chart" id="myChart"></canvas>
+    </div>
+    """
+  end
 
   def spinner(assigns) do
     ~H"""
